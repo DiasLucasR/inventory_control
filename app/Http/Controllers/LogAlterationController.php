4e3d620739dfd69c191d46a2d157dfbafa39ed31
Samuel Nunes <sammoc11@hotmail.com>
2022-05-21 00:54:56 -0300

    Cria migrations, models e controladores para as tabelas products, categories e log_alterations

---
+++ app/Http/Controllers/LogAlterationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LogAlteration;
+use Illuminate\Http\Request;
+
+class LogAlterationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LogAlteration  $logAlteration
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LogAlteration $logAlteration)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LogAlteration  $logAlteration
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LogAlteration $logAlteration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LogAlteration  $logAlteration
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LogAlteration $logAlteration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LogAlteration  $logAlteration
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LogAlteration $logAlteration)
+    {
+        //
+    }
+}
