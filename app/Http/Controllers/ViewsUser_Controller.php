298a33906e882ec817f8340aaf490a09afec21ab
lucasrafa2008 <lucasrafa2008@gmail.com>
2022-05-25 21:06:57 -0300

    Definindo Rotas e Controllers

---
+++ app/Http/Controllers/ViewsUser_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ViewsUser_Controller extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewsCategorias_Controller;
+use App\Http\Controllers\ViewsProdutos_Controller;
+use App\Http\Controllers\ViewsUser_Controller;
+use App\Http\Controllers\ViewsHome_Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app/products/add', [ViewsProdutos_Controller::class, 'adicionar_produtos']);
+Route::get('/app/products/modify', [ViewsProdutos_Controller::class, 'alterar_produtos']);
+Route::get('/app/products/delete', [ViewsProdutos_Controller::class, 'adicionar_produtos']);
+
+Route::get('/app/categorias/add', [ViewsCategorias_Controller::class, 'adicionar_categorias']);
+Route::get('/app/categorias/delete', [ViewsCategorias_Controller::class, 'excluir_categorias']);
+
+Route::get('/app/user/add', [ViewsUser_Controller::class, 'adicionar_categorias']);
+Route::get('/app/user/modify', [ViewsUser_Controller::class, 'adicionar_categorias']);
+Route::get('/app/user/delete', [ViewsUser_Controller::class, 'adicionar_categorias']);
+
+
+Route::get('/app/navbar', [ViewsHome_Controller::class, 'navbar']);
+Route::get('/app/home_usuario', [ViewsHome_Controller::class, 'home_user']);
+
+
+
+
+
+
+
